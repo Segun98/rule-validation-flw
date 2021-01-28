44285f6b4f9ef2d33f83a9e42fa6fa8481c1e93b
Segun <shegunolanitori@gmail.com>
2021-01-28 16:24:06 +0100

    Feat: cleanups

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
+
 //routes imports
 const home = require("./Routes")
 const ruleValidation = require("./Routes/rule_validation")
